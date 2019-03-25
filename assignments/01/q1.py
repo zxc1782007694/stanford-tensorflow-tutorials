e83db12b0840b07e866190337fc00db31b60d2b5
zxc1782007694 <48863104+zxc1782007694@users.noreply.github.com>
2019-03-25 23:01:11 +0800

    Update q1.py

--- assignments/01/q1.py
+++ assignments/01/q1.py
@@ -31,6 +31,11 @@ print(sess.run(out))
 ###############################################################################
 
 # YOUR CODE
+x=tf.random_uniform([], minval=-1, dtype=tf.float32)
+y=tf.random_uniform([], minval=-1, dtype=tf.float32)
+r=tf.case({tf.less(x,y):lambda:tf.add(x,y),tf.greater(x,y):lambda:tf.subtract(x,y)},default=lambda:tf.constant(0.0),exclusive=True)
+sess=tf.Session()
+print(sess.run([x,y,r]))
 
 ###############################################################################
 # 1c: Create the tensor x of the value [[0, -2, -1], [0, 1, 2]] 
@@ -40,7 +45,10 @@ print(sess.run(out))
 ###############################################################################
 
 # YOUR CODE
-
+x=tf.constant([[0, -2, -1], [0, 1, 2]]) 
+y=tf.zeros([2,3],tf.int32)
+r=tf.equal(x,y)
+sess=tf.Session()
 ###############################################################################
 # 1d: Create the tensor x of value 
 # [29.05088806,  27.61298943,  31.19073486,  29.35532951,
@@ -55,6 +63,16 @@ print(sess.run(out))
 ###############################################################################
 
 # YOUR CODE
+x=tf.constant([29.05088806,  27.61298943,  31.19073486,  29.35532951,
+  30.97266006,  26.67541885,  38.08450317,  20.74983215,
+  34.94445419,  34.45999146,  29.06485367,  36.01657104,
+  27.88236427,  20.56035233,  30.20379066,  29.51215172,
+  33.71149445,  28.59134293,  36.05556488,  28.66994858]) 
+y=tf.where(tf.greater(x,30))
+sess=tf.Session()
+print(sess.run(y))
+z=tf.gather(x,y)
+print(sess.run(z))
 
 ###############################################################################
 # 1e: Create a diagnoal 2-d tensor of size 6 x 6 with the diagonal values of 1,
@@ -64,6 +82,10 @@ print(sess.run(out))
 
 # YOUR CODE
 
+x=tf.range(1,7,1)
+y=tf.diag(x)
+sess=tf.Session()
+print(sess.run(y))
 ###############################################################################
 # 1f: Create a random 2-d tensor of size 10 x 10 from any distribution.
 # Calculate its determinant.
@@ -71,6 +93,11 @@ print(sess.run(out))
 ###############################################################################
 
 # YOUR CODE
+x=tf.range(1.0,11.0,1.0)
+y=tf.diag(x)
+z=tf.matrix_determinant(y)
+sess=tf.Session()
+print(sess.run(y))
 
 ###############################################################################
 # 1g: Create tensor x with value [5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9].
@@ -80,6 +107,10 @@ print(sess.run(out))
 
 # YOUR CODE
 
+x=tf.constant([5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9])
+y, idx = tf.unique(x)
+sess=tf.Session()
+print(sess.run(y))
 ###############################################################################
 # 1h: Create two tensors x and y of shape 300 from any normal distribution,
 # as long as they are from the same distribution.
@@ -91,3 +122,9 @@ print(sess.run(out))
 ###############################################################################
 
 # YOUR CODE
+
+x=tf.random_normal([1,300],mean=0.0,stddev=1.0,dtype=tf.float32)
+y=tf.random_normal([1,300],mean=0.0,stddev=1.0,dtype=tf.float32)
+z=tf.cond(tf.reduce_mean(x-y)<0,lambda:tf.reduce_mean(tf.square(x-y)),lambda:tf.reduce_sum(tf.abs(x-y)))
+sess=tf.Session()
+print(sess.run(x-y))
